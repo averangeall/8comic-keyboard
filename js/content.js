e8aeaea693733eb62da9f4341de6c03fef30cc1d
averangeall <averangeall@gmail.com>
2013-06-20 15:57:28 +0800

    Know the direction key

--- js/content.js
+++ js/content.js
@@ -1,3 +1,9 @@
-document.onkeydown = function() {
-    alert('test');
+document.onkeyup = function(evt) {
+    var direction;
+    if(evt.keyCode == 37)
+        direction = 'left';
+    else if(evt.keyCode == 39)
+        direction = 'right';
+    else
+        return;
 };
